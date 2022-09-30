038f7097dbb835d5fe738f4cf7df6adf138bfa93
Dhruv Badaya <dhruvbadaya@gmail.com>
2022-09-30 14:54:33 +0530

    Added Voice Instructions

--- Assets/Scripts/isAllConnected.cs
+++ Assets/Scripts/isAllConnected.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class isAllConnected : MonoBehaviour
 {
-    public bool isUPS1connected = false;
+    public List<GameObject> gameObjects;
+    public List<Vector3> positions;
+    public bool isServerOn = true;
+    [SerializeField] GameObject screen;
+
+    private void Awake()
+    {
+        foreach(GameObject i in gameObjects)
+        {
+            positions.Add(i.transform.position);
+        }
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i< gameObjects.Count; i++)
+        {
+            if (!(gameObjects[i].transform.position == positions[i]))
+            {
+                isServerOn = false;
+                screen.SetActive(false);
+            }
+        }
+    }
 }
