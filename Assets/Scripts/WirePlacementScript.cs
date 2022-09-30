57c1e1289a107a1be0eb340b37d3b5c77678eaf1
lightlessdays <dhruvbadaya@gmail.com>
2022-09-30 09:37:18 +0530

    Added wires

---
+++ Assets/Scripts/WirePlacementScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WirePlacementScript : MonoBehaviour
+{
+    //LOGIC: Whenever a wire is in the peripheral of the object, one end of the wire will
+    //automatically be connected to the object. TADA...  //need wires and hololens for testing.
+}
---
+++ Assets/Scripts/isAllConnected.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isAllConnected : MonoBehaviour
+{
+    public bool isUPS1connected = false;
+}
